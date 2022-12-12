07b7316c4791f3a9b5b4414d8269c27655b5a873
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2022-12-12 16:12:49 +0100

    updated js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const feelingsContainer = document.querySelector('.feelings-container');
 const paragraph = document.createElement('p');
 const solutionsContainer = document.querySelector('.solutions-container');
 const soundItems = document.querySelectorAll('.sound-items');
-const music = new Audio('wo.mp3');
-
+const wohoSound = new Audio('wo.mp3');
+const clickSound = new Audio('https://www.fesliyanstudios.com/play-mp3/387');
 //array for the feelings
 const feelings = [
   'sad',
@@ -25,18 +25,19 @@ const feelings = [
 // array for the solutions
 const solutions = [
   'do a handstand',
-  'paint a dog',
-  'lie down',
-  'scream ',
-  'cut your hair',
+  'wash cotton candy',
+  'jump for 10 hours',
+  'loading.....',
+  'wash your headphones',
   'buy a cool shirt',
   'error error error',
   'we dont know',
   'backflip',
 ];
 
-// event-listener for the first button , choose random index from feelings-array, append paragraph that i created into feelings-container
+// event-listener for the first button , choose random index from feelings-array, append paragraph that i created into feelings-container and play a little sound
 buttonFirst.addEventListener('click', () => {
+  clickSound.play();
   const random = Math.floor(Math.random() * feelings.length);
 
   for (i = 0; i < feelings.length; i++) {
@@ -47,8 +48,9 @@ buttonFirst.addEventListener('click', () => {
   }
 });
 
-// event-listener for second button, choose random index from solutions-array, append paragraph that i created into solutions-container
+// event-listener for second button, choose random index from solutions-array, append paragraph that i created into solutions-container and play a little sound
 buttonSecond.addEventListener('click', () => {
+  clickSound.play();
   const random = Math.floor(Math.random() * solutions.length);
 
   for (i = 0; i < solutions.length; i++) {
@@ -59,10 +61,9 @@ buttonSecond.addEventListener('click', () => {
   }
 });
 
+// when you click on a elemnent thats a sounditem, play the sound
 soundItems.forEach((sounditem) => {
-  sounditem.addEventListener('click', () => {
-    music.play();
+  sounditem.addEventListener('mouseover', () => {
+    wohoSound.play();
   });
 });
-
-// const myTimeout = setTimeout(myGreeting, 5000);
